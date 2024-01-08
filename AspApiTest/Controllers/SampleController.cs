d757f2f5688365e6487e0a59179dc2e5f4386e48
junpei.nagai <jumpei603@gmail.com>
2024-01-08 20:26:19 +0900

    ソースコード追加

---
+++ AspApiTest/Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspApiTest.Controllers {
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase {
+
+        private readonly ILogger<SampleController> _logger;
+
+        public SampleController(ILogger<SampleController> logger) {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// https://localhost:7105/sample にアクセスで、「request success」を返却
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name ="sample")]
+        public String Sample() {
+            return "request success";
+        }
+    }
+}
